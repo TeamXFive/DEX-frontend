add0bf54ab06b18aa2cdc1f81cc1e8bb2a0b78e5
Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>
2024-08-22 16:27:04 +0000

    [chore] Testando roteamento do react-router
    Signed-off-by: Lorenzo O. Zimbres <97912499+LoriaLawrenceZ@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import React, { useEffect, useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import React, { useEffect,  } from "react";
 import "./App.css";
 import Chat from "./pages/Chat/Chat";
 import Home from "./pages/Home/Home";
