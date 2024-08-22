cf62dcce1fcd95a029d7cb85c79e4c3da2f187ea
Ivan Ramos Biagioni <140509979+ivan-maze@users.noreply.github.com>
2024-08-22 20:36:43 -0300

    [feat] starting heade

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,9 @@
-import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./App.css";
+import Header from "./components/header/header";
+import React from "react";
 
 export function App() {
-  const [currentPage, setCurrentPage] = useState("");
 
-  useEffect(() => {
-    console.log("INITIAL LOAD");
-  }, []);
-
-  useEffect(() => {
-    setCurrentPage(window.location.pathname.replace("/", ""));
-  }, []);
-
-  return (
-    <React.Fragment>
-      {/* Placeholder Header */}
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/chat">Chat</Link>
-      </div>
-      {/* Placeholder Header */}
-    </React.Fragment>
-  );
+  return (<Header/>) 
 }
