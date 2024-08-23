6db35a54536f49bce23cb37ff3c605c7e1c75227
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-08-23 01:09:58 -0300

    [refactor] Reestruturando para inclusão da tela de conta
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,31 @@ import React from "react";
 import { Link, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Chat from "./pages/Chat/Chat";
+import Account from "./pages/Account/Account.jsx";
 
 export function App() {
   return (
-    <React.Fragment>
-      {/* Placeholder Header */}
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/sobre">Sobre</Link>
-        <Link to="/login">Login</Link>
-        <Link to="/chat">Chat</Link>
+      <div className="page">
+          <React.Fragment>
+          {/* Placeholder Header */}
+          <header className="page-header">
+              <Link to="/">Home</Link>
+              <Link to="/sobre">Sobre</Link>
+              <Link to="/chat">Chat</Link>
+              <Link to="/account">Login</Link>
+          </header>
+          {/* Placeholder Header */}
+              <div className="page-content">
+                  <Routes>
+                      <Route path="/" element={<Home />} />
+                      <Route path="/sobre" element={<div>Sobre</div>} />
+                      <Route path="/chat" element={<Chat />} />
+                      <Route path="/account" element={<Account />} />
+                      <Route path="*" element={<Navigate to="/" />} />
+                  </Routes>
+                  <Chat type="widget" />
+              </div>
+        </React.Fragment>
       </div>
-      {/* Placeholder Header */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/sobre" element={<div>Sobre</div>} />,
-        <Route path="/login" element={<div>Login</div>} />,
-        <Route path="/chat" element={<Chat />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-      <Chat type="widget" />
-    </React.Fragment>
   );
 }
