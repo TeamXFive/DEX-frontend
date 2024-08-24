59d96bd6f2848ed0a89c8cac9faf9988a4b399e9
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-08-24 00:35:09 -0300

    [chore] Estilizando
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,50 @@
 import "./App.css";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link, Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Chat from "./pages/Chat/Chat";
 import Account from "./pages/Account/Account.jsx";
 
 export function App() {
+    const [scrollDirection, setScrollDirection] = useState("up");
+    const [lastScrollY, setLastScrollY] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const currentScrollY = window.scrollY;
+
+            if (currentScrollY > lastScrollY) {
+                setScrollDirection("down");
+            } else {
+                setScrollDirection("up");
+            }
+
+            setLastScrollY(currentScrollY);
+        };
+
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, [lastScrollY]);
+
   return (
       <div className="page">
           <React.Fragment>
               {/* Placeholder Header */}
-              <header className="page-header">
-                  <nav className="glass-effect">
-                      <Link to="/">HOME</Link>
-                      <Link to="/sobre">SOBRE</Link>
-                      <Link to="/chat">CHAT</Link>
-                      <Link to="/account">LOGIN</Link>
+              <header className={`page-header glass-effect ${scrollDirection === "down" ? "hide-header" : ""}`}>
+                  <figure className="header-logo">
+                      <img src="src/assets/icons/chat.svg" alt="Logo Header"/>
+                  </figure>
+
+                  <nav className="header-nav">
+                      <div className="header-nav-links">
+                          <Link to="/">HOME</Link>
+                          <Link to="/sobre">SOBRE</Link>
+                          <Link to="/chat">CHAT</Link>
+                          <Link to="/account">LOGIN</Link>
+                      </div>
                   </nav>
               </header>
               {/* Placeholder Header */}
