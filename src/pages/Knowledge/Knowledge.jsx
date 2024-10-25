af5aff3985a5ec6def50ee9395fc0ef17852292b
LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>
2024-10-25 18:36:33 -0300

    [fix] localhost for api endpoints
    Signed-off-by: LoriaLawrenceZ <lorenzozimbresfilmmaker@gmail.com>

--- src/pages/Knowledge/Knowledge.jsx
+++ src/pages/Knowledge/Knowledge.jsx
@@ -92,8 +92,7 @@ function Knowledge() {
                 
                 try {
                     
-                    // const response = await fetch('http://localhost:3000/api/upload', {
-                    const response = await fetch('https://dex-backend-vercel-steel.vercel.app/api/upload', {
+                    const response = await fetch('http://localhost:3000/api/upload', {
                         method: 'POST',
                         body: formData
                     });
@@ -133,8 +132,7 @@ function Knowledge() {
 
     const getFiles = async () => {
         try {
-            const response = await fetch('https://dex-backend-vercel-steel.vercel.app/api/file_retrieval', {
-            // const response = await fetch('http://localhost:3000/api/file_retrieval', {
+            const response = await fetch('http://localhost:3000/api/file_retrieval', {
                 method: 'POST',
             });
             if (!response.ok) {
