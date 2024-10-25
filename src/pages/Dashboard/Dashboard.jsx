caf43d40aaf9c2924cbea41889a9186af9702142
HenriqueSaniW <108810733+HenriqueSaniW@users.noreply.github.com>
2024-10-25 20:42:01 -0300

    Alterações dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -6,6 +6,7 @@ import Person from  '../../assets/icons/person.svg?react'
 import Home from  '../../assets/icons/home.svg?react'
 import Sobre from  '../../assets/icons/sobre.svg?react'
 import Visao from  '../../assets/icons/visao.svg?react'
+import { Link } from "react-router-dom";
 
 
 
@@ -16,9 +17,11 @@ function Dashboard(){
         <>
             <main className="containerGeral">
                 <section className="containerLeft">
+                    <Link to='/'>
                     <div  className="logo">
                         <img src="../../../public/images/fiap_logo.png" alt="" />
                     </div>
+                    </Link>
                     <div className="ContainerListaPages">
                         <ul className="listaPages">
                             <li>
@@ -66,8 +69,17 @@ function Dashboard(){
                         <div className="insetContainerLeft">
                             <div className="graphicSuport"></div>
                             <div className="dadosLow">
-                                <div></div>
-                                <div></div>
+                                <div className="containerDados">
+                                    <h4 className="titleDados" style={{color: 'white'}}>Chamados Concluídos</h4>
+                                    <div className="numerosDados">
+                                        <p>57</p>
+                                        <span className="spanConcluido">+14,7%</span>
+                                    </div>
+                                </div>
+                                <div className="containerDados">
+                                <h4 className="titleDados" style={{color: 'white'}}>Chamados Para N3</h4>
+                                <div className="numerosDados"><p>06</p><span className="spanN3">-4,2%</span></div>
+                                </div>
                             </div>
                         </div>
                         <div className="insetContainerRight">
@@ -85,17 +97,58 @@ function Dashboard(){
                                     </div>
                                     <div className="contentTable">
                                         <div className="lineContent">
-                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p>José Othon.</p></div>
+                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p style={{maxWidth: '11ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>José Othon</p></div>
                                             <p>Oct 25, 2024</p>
-                                            <p>Como arrumar uma impresso...</p>
+                                            <p style={{maxWidth: '24ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>Como arrumar uma impressora que precisa</p>
                                             <div className="InProcess">In Process</div>
+                                            <button>See Details</button>
+                                        </div>
+                                        <div className="lineContent">
+                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p style={{maxWidth: '11ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>José Othon</p></div>
+                                            <p>Oct 25, 2024</p>
+                                            <p style={{maxWidth: '24ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>Como arrumar uma impressora que precisa</p>
+                                            <div className="Done">Done</div>
+                                            <button>See Details</button>
+                                        </div>
+                                        <div className="lineContent">
+                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p style={{maxWidth: '11ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>José Othon</p></div>
+                                            <p>Oct 25, 2024</p>
+                                            <p style={{maxWidth: '24ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>Como arrumar uma impressora que precisa</p>
+                                            <div className="Canceled">Canceled</div>
+                                            <button>See Details</button>
+                                        </div>
+                                        <div className="lineContent">
+                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p style={{maxWidth: '11ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>José Othon</p></div>
+                                            <p>Oct 25, 2024</p>
+                                            <p style={{maxWidth: '24ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>Como arrumar uma impressora que precisa</p>
+                                            <div className="N3">N3</div>
+                                            <button>See Details</button>
+                                        </div>
+                                        <div className="lineContent">
+                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p style={{maxWidth: '11ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>José Othon</p></div>
+                                            <p>Oct 25, 2024</p>
+                                            <p style={{maxWidth: '24ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>Como arrumar uma impressora que precisa</p>
+                                            <div className="Done">Done</div>
+                                            <button>See Details</button>
+                                        </div>
+                                        <div className="lineContent">
+                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p style={{maxWidth: '11ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>José Othon</p></div>
+                                            <p>Oct 25, 2024</p>
+                                            <p style={{maxWidth: '24ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>Como arrumar uma impressora que precisa</p>
+                                            <div className="Canceled">Canceled</div>
+                                            <button>See Details</button>
+                                        </div>
+                                        <div className="lineContent">
+                                            <div style={{display: 'flex', gap: '5px'}}><Person/> <p style={{maxWidth: '11ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>José Othon</p></div>
+                                            <p>Oct 25, 2024</p>
+                                            <p style={{maxWidth: '24ch',overflow:'hidden', whiteSpace: 'nowrap', textOverflow: 'ellipsis'}}>Como arrumar uma impressora que precisa</p>
+                                            <div className="N3">N3</div>
                                             <button>See Details</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className='graphicPerfomance'>
-                                
+                            <div className='graphicPerformance'>
                             </div>
                         </div>
                     </div>
