11315ed48f41ae492dc2098aa2889bb4d43d719d
carolinejk <carolinenunesjk@gmail.com>
2024-10-24 23:49:50 -0300

    feat: putting localhost

--- src/pages/Knowledge/Knowledge.jsx
+++ src/pages/Knowledge/Knowledge.jsx
@@ -92,8 +92,8 @@ function Knowledge() {
                 
                 try {
                     
-                    // const response = await fetch('http://localhost:3000/api/upload', {
-                    const response = await fetch('https://dex-backend-two.vercel.app/api/upload', {
+                    const response = await fetch('http://localhost:3000/api/upload', {
+                    // const response = await fetch('https://dex-backend-two.vercel.app/api/upload', {
                         method: 'POST',
                         body: formData
                     });
@@ -133,8 +133,8 @@ function Knowledge() {
 
     const getFiles = async () => {
         try {
-            const response = await fetch('https://dex-backend-two.vercel.app/api/file_retrieval', {
-            // const response = await fetch('http://localhost:3000/api/file_retrieval', {
+            // const response = await fetch('https://dex-backend-two.vercel.app/api/file_retrieval', {
+            const response = await fetch('http://localhost:3000/api/file_retrieval', {
                 method: 'POST',
             });
             if (!response.ok) {
